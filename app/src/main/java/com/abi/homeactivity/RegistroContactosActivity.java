d55ff947229dd6b4efb8f2187af3b120d6426b86
Carlos Munive <cmuniveo1400@alumno.ipn.mx>
2021-05-19 21:51:13 -0500

    Pantalla de registro de contcatos

---
+++ app/src/main/java/com/abi/homeactivity/RegistroContactosActivity.java
@@ -0,0 +1,15 @@
+package com.abi.homeactivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistroContactosActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_registro_contactos);
+    }
+}
